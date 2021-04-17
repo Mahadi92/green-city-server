dfb689e4ac1a542c04cdc318f0b529c8d9c8b6c8
Mahadi92 <mahadi.hasan.937524.mh@gmail.com>
2021-04-17 19:06:11 +0600

    Sevices create and read done

--- index.js
+++ index.js
@@ -17,8 +17,29 @@ app.use(cors());
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
     const treesCollection = client.db("greenCity").collection("trees");
-    console.log('successfully');
+    const servicesCollection = client.db("greenCity").collection("services");
 
+
+    // Add services
+    app.post('/addServices', (req, res) => {
+        const services = req.body;
+        console.log(services);
+        servicesCollection.insertOne(services)
+            .then(results => {
+                res.send(results.insertedCount > 0)
+
+                console.log("🚀 ~ file: index.js ~ line 29 ~ app.post ~ results", results)
+            })
+    })
+
+    // Read services data
+    app.get('/services', (req, res) => {
+        servicesCollection.find({})
+            .toArray((err, services) => {
+                res.send(services)
+                console.log(err);
+            })
+    })
 
 
 });
@@ -26,6 +47,6 @@ client.connect(err => {
 
 
 app.get('/', function (req, res) {
-    res.send('hello world')
+    res.send('Welcome to Green City server')
 })
 app.listen(process.env.PORT || 5000);
